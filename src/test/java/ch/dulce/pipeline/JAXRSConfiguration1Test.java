7c91e116c1c3541e7bb525c1459549b71b0f7ee8
Antonio <dolce.antonio@gmail.com>
2018-12-08 16:21:09 +0100

    added tests

---
+++ src/test/java/ch/dulce/pipeline/JAXRSConfiguration1Test.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.dulce.pipeline;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ *
+ * @author nino
+ */
+public class JAXRSConfiguration1Test {
+
+    public JAXRSConfiguration1Test() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+        assertTrue(true);
+    }
+
+}
